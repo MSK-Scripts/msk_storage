be6b5f1cb99d2172dbb290a420d58b76d339f3ab
Musiker15 <info@musiker15.de>
2024-08-19 11:21:42 +0200

    Update v1.2.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ games { 'gta5' }
 author 'Musiker15 - MSK Scripts'
 name 'msk_storage'
 description 'Storage System'
-version '1.2.0'
+version '1.2.1'
 
 lua54 'yes'
 
--- integration/server_integration.lua
+++ integration/server_integration.lua
@@ -1,21 +1,3 @@
-registerStashes = function()
-    if Config.Inventory == 'chezza_v3' or Config.Inventory == 'chezza_v4' then return end
-    while not database do Wait(100) end
-
-    if Config.Inventory == 'ox_inventory' then
-        for identifier, storage in pairs(database) do
-            local inventory = exports.ox_inventory:GetInventory(storage.uniqueId .. identifier, false)
-
-            if not inventory then
-                registerStash(identifier, storage)
-            end
-        end
-    elseif Config.Inventory == 'custom' then
-        -- Add your own inventory here
-    end
-end
-registerStashes()
-
 registerStash = function(identifier, storage)
     if Config.Inventory == 'ox_inventory' then
         exports.ox_inventory:RegisterStash(
@@ -40,3 +22,21 @@ upgradeStash = function(identifier, storage)
         -- Add your own inventory here
     end
 end
+
+registerStashes = function()
+    if Config.Inventory == 'chezza_v3' or Config.Inventory == 'chezza_v4' then return end
+    while not database do Wait(100) end
+
+    if Config.Inventory == 'ox_inventory' then
+        for identifier, storage in pairs(database) do
+            local inventory = exports.ox_inventory:GetInventory(storage.uniqueId .. identifier, false)
+
+            if not inventory then
+                registerStash(identifier, storage)
+            end
+        end
+    elseif Config.Inventory == 'custom' then
+        -- Add your own inventory here
+    end
+end
+registerStashes()
